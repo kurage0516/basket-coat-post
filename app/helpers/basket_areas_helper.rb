1698cb0f0c7222fb5c0935d92f3d2697b7e742f7
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-03-03 22:15:44 +0900

    ユーザー管理機能実装。Rubocop済

---
+++ app/helpers/basket_areas_helper.rb
@@ -0,0 +1,2 @@
+module BasketAreasHelper
+end
---
+++ test/controllers/basket_areas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BasketAreasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
