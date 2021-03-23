ebc125deaf3dd86fdb9b6a8a084839c1443c79ea
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-03-23 19:40:26 +0900

    単体モデルテスト完了

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :comment do
+<<<<<<< Updated upstream
     
+=======
+    text {Faker::Lorem.sentence}
+    association :user
+    association :basket_area
+>>>>>>> Stashed changes
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it 'textが存在していれば保存できること' do
+      expect(@comment).to be_valid
+    end
+
+    it 'textが空だと保存できない' do
+      @comment.text = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('User must exist')
+    end
+
+    it 'basket_areaが紐付いていないと保存できないこと' do
+      @comment.basket_area = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('Basket area must exist')
+    end
+  end
 end
